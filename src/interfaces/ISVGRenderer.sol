7ae70fcee0bb252a368402f132474f56b8008bf2
Patate <patate@joepegs.com>
2023-02-21 20:55:14 +0100

    add Nouns contracts for SVG rendering

---
+++ src/interfaces/ISVGRenderer.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/// @author NounsDAO: https://github.com/nounsDAO/nouns-monorepo
+/// @title Interface for SVGRenderer
+
+/**
+ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░██░░░████░░██░░░████░░░ *
+ * ░░██████░░░████████░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░██░░██░░░████░░██░░░████░░░ *
+ * ░░░░░░█████████░░█████████░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
+ *
+ */
+
+pragma solidity ^0.8.6;
+
+interface ISVGRenderer {
+    struct Part {
+        bytes image;
+        bytes palette;
+    }
+
+    struct SVGParams {
+        Part[] parts;
+        string background;
+    }
+
+    function generateSVG(SVGParams memory params) external view returns (string memory svg);
+
+    function generateSVGPart(Part memory part) external view returns (string memory partialSVG);
+
+    function generateSVGParts(Part[] memory parts) external view returns (string memory partialSVG);
+}
