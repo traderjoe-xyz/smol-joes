254ff72a594b4cd43de87b96e0c52a93718c6a8d
Patate <patate@joepegs.com>
2023-04-13 16:58:41 +0200

    update natspecs and comments

--- contracts/interfaces/ISmolJoes.sol
+++ contracts/interfaces/ISmolJoes.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.13;
 
 import {IOZNFTBaseUpgradeable} from "@traderjoe-xyz/nft-base-contracts/src/OZNFTBaseUpgradeable.sol";
 
-import {ISmolJoeSeeder} from "./ISmolJoeSeeder.sol";
 import {ISmolJoeDescriptorMinimal} from "./ISmolJoeDescriptorMinimal.sol";
 import {ISmolJoeSeeder} from "./ISmolJoeSeeder.sol";
 
@@ -11,10 +10,23 @@ import {ISmolJoeSeeder} from "./ISmolJoeSeeder.sol";
  * @title Interface for SmolJoes
  */
 interface ISmolJoes is IOZNFTBaseUpgradeable {
-    event DescriptorUpdated(ISmolJoeDescriptorMinimal descriptor);
-    event SeederUpdated(ISmolJoeSeeder seeder);
+    event DescriptorUpdated(address descriptor);
+    event SeederUpdated(address seeder);
+    event WorkshopUpdated(address workshop);
 
-    function dataURI(uint256 tokenId) external returns (string memory);
+    error SmolJoes__Unauthorized();
+    error SmolJoes__InvalidAddress();
+    error SmolJoes__InexistentToken(uint256 tokenId);
+
+    function descriptor() external view returns (ISmolJoeDescriptorMinimal);
+
+    function seeder() external view returns (ISmolJoeSeeder);
+
+    function workshop() external view returns (address);
+
+    function getTokenSeed(uint256 tokenId) external view returns (ISmolJoeSeeder.Seed memory);
+
+    function dataURI(uint256 tokenId) external view returns (string memory);
 
     function setDescriptor(ISmolJoeDescriptorMinimal descriptor) external;
 
