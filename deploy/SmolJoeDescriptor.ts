4c10c85dd0e84ae65b1781564f2118d2c47a0c18
Mathieu <85969303+Mathieu-Be@users.noreply.github.com>
2023-04-14 20:07:05 +0200

    Add deploy scripts and tasks (#3)
    * add deployment scripts
    
    * add hardhat tasks

---
+++ deploy/SmolJoeDescriptor.ts
@@ -0,0 +1,33 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, run } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const renderer = (await deployments.get("SVGRenderer")).address;
+
+  // Art address will be updated in the Art deploy script
+  const deployResult = await deploy("SmolJoeDescriptor", {
+    from: deployer,
+    args: ["0x0000000000000000000000000000000000000001", renderer],
+    log: true,
+    autoMine: true,
+  });
+
+  if (hre.network.name !== "hardhat") {
+    try {
+      await run("verify:verify", {
+        address: deployResult.address,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+};
+
+export default func;
+
+func.tags = ["SmolJoeDescriptor"];
+func.dependencies = ["SVGRenderer"];
