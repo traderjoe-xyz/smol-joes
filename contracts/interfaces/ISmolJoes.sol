b4a2c253d53fb53f72b556f2a8b8ad228294dda9
Patate <patate@joepegs.com>
2023-04-18 09:50:21 +0200

    set solidity version used to 0.8.13

--- contracts/interfaces/ISmolJoes.sol
+++ contracts/interfaces/ISmolJoes.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.6;
+pragma solidity 0.8.13;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {ISmolJoeSeeder} from "./ISmolJoeSeeder.sol";
