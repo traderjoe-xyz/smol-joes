d60215e09b0a52f6476a542ad046fdd27ddb354e
Patate <patate@joepegs.com>
2023-04-26 09:56:10 +0200

    rename folders

--- script/00_BaseScript.s.sol
+++ script/00_BaseScript.s.sol
@@ -3,13 +3,13 @@ pragma solidity 0.8.13;
 
 import "forge-std/Script.sol";
 
-import {SmolJoes, ISmolJoes} from "contracts/SmolJoes.sol";
-import {SmolJoeDescriptor, ISmolJoeDescriptor} from "contracts/SmolJoeDescriptor.sol";
-import {SmolJoeSeeder, ISmolJoeSeeder} from "contracts/SmolJoeSeeder.sol";
-import {SmolJoeWorkshop, ISmolJoeWorkshop} from "contracts/SmolJoeWorkshop.sol";
-import {SVGRenderer} from "contracts/SVGRenderer.sol";
-import {SmolJoeArt, ISmolJoeArt} from "contracts/SmolJoeArt.sol";
-import {Inflator} from "contracts/Inflator.sol";
+import {SmolJoes, ISmolJoes} from "src/SmolJoes.sol";
+import {SmolJoeDescriptor, ISmolJoeDescriptor} from "src/SmolJoeDescriptor.sol";
+import {SmolJoeSeeder, ISmolJoeSeeder} from "src/SmolJoeSeeder.sol";
+import {SmolJoeWorkshop, ISmolJoeWorkshop} from "src/SmolJoeWorkshop.sol";
+import {SVGRenderer} from "src/SVGRenderer.sol";
+import {SmolJoeArt, ISmolJoeArt} from "src/SmolJoeArt.sol";
+import {Inflator} from "src/Inflator.sol";
 
 contract BaseScript is Script {
     using stdJson for string;
